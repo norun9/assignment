ce2bec7fc01086a105c457989476a0b6a5a21b22
tomoyaueno29 <u.t012807@gmail.com>
2020-06-04 16:23:22 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
+	"fmt"
+	"strings"
 )
 
 func main() {
 	body, _ := ioutil.ReadAll(os.Stdin)
-	fmt.Println(string(body))
+	b := string(body)
+	//"."で区切って同じ文字の配列を作成
+	slice := strings.Split(b, ".")
+
+	lists := [][]string{}
+	//最後に空白が入るため-1をしている(要リファクタリング)
+	for i := 0; i<len(slice)-1; i++{
+		sameStrSlice := strings.Split(slice[i], " ")
+		if sameStrSlice[0] == ""{
+			sameStrSlice = append(sameStrSlice[:0], sameStrSlice[1:]...)
+		}
+		//fmt.Println(len(sameStrSlice))
+		//fmt.Println(sameStrSlice)
+		lists = append(lists, sameStrSlice)
+	}
+	fmt.Println(lists)
+}
+
+type Words struct {
+	Name string
+	Length int
+}
+
+func (w Words) String() string{
+	return w.Name
+}
+
+type ByLen []Words
+
+func (l ByLen) Len() int {
+	return len(l)
 }
